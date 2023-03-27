19c9b6f2a3ed63925c09a8859df97f9591797527
MoshDedov <damnthisisso@hotmail.com>
2023-03-27 04:18:07 +0300

    first commit

---
+++ src/login.js
@@ -0,0 +1,22 @@
+import './login.css';
+
+function Login() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <a>Заходи че встал</a>
+        <form className='form'>
+          <input></input>
+          <input></input>
+          <input></input>
+          <div className='logreg'>
+            <a href=''>Войти</a>
+            <a href=''>Зарегистрироваться</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
